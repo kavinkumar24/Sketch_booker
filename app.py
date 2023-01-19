98b553a75e1ae5628ede1182da3406c584ebd8c5
KAVIN KUMAR P <103486304+kavinkumar24@users.noreply.github.com>
2023-01-19 21:31:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
 from distutils.log import error
+from tkinter.tix import NoteBook
+from turtle import width
 from typing import final
 import cv2 #for image processing
 import numpy as np #to store image
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
 from PIL import Image
 import streamlit as st
 from pytesseract import image_to_string
 import pytesseract
 from PIL import UnidentifiedImageError
+import streamlit.components.v1 as components
+from streamlit_option_menu import option_menu
+
+import io
+with st.sidebar:
+    select = option_menu(
+            menu_title="Main Menu",
+            options = ["Pencil Scatcher","cartoon Image converter"],
+            menu_icon="cast")
+    page_bg_img = f"""
+        <style>
+        .menu .container-xxl[data-v-4323f8ce]{{
+            background-color:white;
+            color:black; 
+        }} 
+        [data-testid="stSidebar"] {{
+        background-color:#d5e1ed;
+        color:white;    
+        }}
+        </style>
+        """ 
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    base='light'  
+
+st.image('sketch.png',width=300,)
 
 def pencilSketch(input_image):
     image_grey = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
     image_invert = cv2.bitwise_not(image_grey)
     image_smoothing = cv2.GaussianBlur(
-        image_invert, (21, 21), sigmaX=0, sigmaY=0)
+        image_invert, (101, 101), sigmaX=0, sigmaY=0)
     global final_image
     final_image = cv2.divide(image_grey, 255-image_smoothing, scale=256)
     return(final_image)
@@ -22,11 +52,12 @@ def cartoon(input_image):
         image_grey = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
         image_invert = cv2.bitwise_not(image_grey)
         image_smoothing = cv2.GaussianBlur(
-            image_invert, (71, 71), sigmaX=0, sigmaY=0)
+            image_invert, (21, 21), sigmaX=0, sigmaY=0)
         final_image = cv2.divide(image_grey, 255-image_smoothing, scale=256)
         smoothGrayScale = cv2.medianBlur(image_grey, 5)
         ReSized3 = cv2.resize(smoothGrayScale, (960, 540))
         #plt.imshow(ReSized3, cmap='gray')
+
         getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, 
         cv2.ADAPTIVE_THRESH_MEAN_C, 
         cv2.THRESH_BINARY, 17, 10)
@@ -35,21 +66,26 @@ def cartoon(input_image):
         colorImage = cv2.bilateralFilter(input_image, 9, 500, 500)
         ReSized5 = cv2.resize(colorImage, (1060, 840))
         #plt.imshow(ReSized5, cmap='gray')
+
         cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
         ReSized6 = cv2.resize(cartoonImage, (960, 540))
         #plt.imshow(ReSized6, cmap='gray'
         st.image(cartoonImage)
        
-selected_box = st.sidebar.selectbox(
-'Choose one of the following',
-('Pencil Scatcher','cartoon Image converter'))
-if selected_box == 'Pencil Scatcher':
+
+
+
+    
+    
+
+if select == 'Pencil Scatcher':
     st.title("PencilScatcher App")
     st.write('This web app is to help convert your photos to realistic Pencil Sketches')
+
+
     image = st.file_uploader("Upload your photo", type=['jpeg', 'jpg', 'png'])
     if image is None:
         st.write("You have not uploaded any image")
-        st.write("Upload any type of images, but jpg images give better results")
     else:
         input_image = Image.open(image)
         final_img = pencilSketch(np.array(input_image))
@@ -57,9 +93,11 @@ if selected_box == 'Pencil Scatcher':
         st.image(image, use_column_width=True)
         st.write("Output Pencil Photo")
         st.image(final_img, use_column_width=True)
-        
-if selected_box == 'cartoon Image converter':
+
+
+if select == 'cartoon Image converter':
     image_cartoon = st.file_uploader("Upload your ", type=['jpeg', 'jpg'])
+   
     if image_cartoon is None:
         st.header("support images only in jpg")
     else:
